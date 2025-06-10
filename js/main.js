331d4c9e9f853e73a64da0d0563581f674d8c61e
alejandrorumi-dev <alejandrorumi.dev@gmail.com>
2025-06-10 12:55:19 +0200

    Add link to display hamburger menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+// Importar el archivo de las navegaciones
+
+import { setupHamburgerMenu } from '../js/navigation.js';
+document.addEventListener('DOMContentLoaded', () => {
+  setupHamburgerMenu();
+});
+
 // Importar el archivo de las animaciones
 
 import { animateTitle } from '../js/animations.js';
